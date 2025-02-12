e5522fd7b853d3e93bc21eaba3a41f6fb81353de
Javeriya00 <javeriyafathima111@gmail.com>
2025-02-12 22:29:11 +0530

    Initial

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "my-aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
